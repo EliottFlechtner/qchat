7ff6649cfc2512c9eea2078c58b43d2ef7cfefe3
Eliott Flechtner (ING2, QUANTUM) <eliott.flechtner@epita.fr>
2025-08-01 17:55:01 +0900

    feat: implement KEM keypair generation and message encryption/decryption functions

--- client/crypto/kem.py
+++ client/crypto/kem.py
@@ -1,22 +1,28 @@
 import oqs, os
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
+KEM_ALGORITHM = "Kyber512"
 
-def generate_keypair():
-    with oqs.KeyEncapsulation("Kyber512") as kem:
+
+def generate_kem_keypair():
+    """Generates a KEM keypair using the specified algorithm.
+    Returns:
+        tuple: (public_key, private_key) where both are bytes.
+    """
+    with oqs.KeyEncapsulation(KEM_ALGORITHM) as kem:
         pub = kem.generate_keypair()
         priv = kem.export_secret_key()
     return pub, priv
 
 
 def encapsulate_key(pubkey: bytes):
-    with oqs.KeyEncapsulation("Kyber512") as kem:
+    with oqs.KeyEncapsulation(KEM_ALGORITHM) as kem:
         ciphertext_kem, shared_secret = kem.encap_secret(pubkey)
     return ciphertext_kem, shared_secret
 
 
 def decapsulate_key(encapsulated: bytes, privkey: bytes):
-    with oqs.KeyEncapsulation("Kyber512", secret_key=privkey) as kem:
+    with oqs.KeyEncapsulation(KEM_ALGORITHM, secret_key=privkey) as kem:
         shared_secret = kem.decap_secret(encapsulated)
     return shared_secret
 
