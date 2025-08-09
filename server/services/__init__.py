1121221257534ff40d8976073c1daa66776b2d21
EliottFlechtner <eliott.flechtner@epita.fr>
2025-08-09 17:20:59 +0900

    feat: add ConversationService for managing conversation creation and retrieval

--- server/services/__init__.py
+++ server/services/__init__.py
@@ -3,12 +3,14 @@
 from .base import BaseService
 from .user_service import UserService
 from .message_service import MessageService
+from .conversation_service import ConversationService
 from .websocket_service import WebSocketService, websocket_service
 
 __all__ = [
     "BaseService",
     "UserService",
     "MessageService",
+    "ConversationService",
     "WebSocketService",
     "websocket_service",
 ]
